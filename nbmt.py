74fb0d28143f0363814edb07effe49b6002d7915
AbMedhat <abdullah.medhat.salah@gmail.com>
2018-08-18 15:04:07 +0200

    Changes 1. python3 adaptation 2. implementing functions 3. updating .gitignore

--- nbmt.py
+++ nbmt.py
@@ -3,85 +3,107 @@ import time
 import os
 import json
 
-cisco_ios = get_network_driver('ios')
-cisco_iosxr = get_network_driver('iosxr')
-juniper = get_network_driver('junos')
-
 hosts = json.load(open('hosts.json'))
 
-if not os.path.exists('running-config'):
-    print('Creating "running-config" Directory')
-    os.makedirs('running-config')
+def check_main_directories():
+    if not os.path.exists('running-config'):
+        print('Creating "running-config" Directory')
+        os.makedirs('running-config')
 
+def get_credentials(hosts, host):
+    if "username" and "password" in hosts["devices"][host].keys():
+        username = hosts["devices"][host]["username"]
+        password = hosts["devices"][host]["password"]
+    elif "username" and "password" in hosts["creds"]:
+        username = hosts["creds"]["username"]
+        password = hosts["creds"]["password"]
+    return username,password
 
-for host in hosts["devices"].keys():
-    hostname = host
-    if "state" in hosts["devices"][host].keys() and hosts["devices"][host]["state"] == "up":
-        
-        if "username" and "password" in hosts["devices"][host].keys():
-            username = hosts["devices"][host]["username"]
-            password = hosts["devices"][host]["password"]
-        elif "username" and "password" in hosts["creds"]:
-            username = hosts["creds"]["username"]
-            password = hosts["creds"]["password"]
-        else:
-            print(">>> Cannot Find Valid Credentials <<<")
-            print("===============================================================================")
-            continue
+def get_napalm_device(hosts,host):
+    if hosts["devices"][host]["driver"] == 'cisco_ios':
+        cisco_ios = get_network_driver('ios')
+        device = cisco_ios(hosts["devices"][host]["ip"], username, password)
+    elif hosts["devices"][host]["driver"] == 'cisco_iosxr':
+        cisco_iosxr = get_network_driver('iosxr')
+        device = cisco_iosxr(hosts["devices"][host]["ip"], username, password)
+    elif hosts["devices"][host]["driver"] == 'juniper':
+        juniper = get_network_driver('junos')
+        device = juniper(hosts["devices"][host]["ip"], username, password)
+    return device
 
-        if hosts["devices"][host]["driver"] == 'cisco_ios':
-            device = cisco_ios(hosts["devices"][host]["ip"], username, password)
-        elif hosts["devices"][host]["driver"] == 'cisco_iosxr':
-            device = cisco_iosxr(hosts["devices"][host]["ip"], username, password)
-        elif hosts["devices"][host]["driver"] == 'juniper':
-            device = juniper(hosts["devices"][host]["ip"], username, password)
-        else:
-            print(">>> Unkown Device type <<< \n")
-            print("===============================================================================")
-            continue
-
-    elif "state" in hosts["devices"][host].keys() and hosts["devices"][host]["state"] == "down":
-        print(">>> Skippping Host %s \n") %hostname
-        print("===============================================================================")
-        continue
+def get_host_state(hosts,host):
+    if "state" in hosts["devices"][host].keys():
+        return hosts["devices"][host]["state"]
     else:
-        print("Undefined State for Host %s \n") %hostname
-        print("===============================================================================")
-        continue
+        return "no-state"
 
-    try:
+def get_running_config(device):
         device.open()
-        print("<<<< Fetching running-config for Device %s >>>>") %hostname
+        print("<<<< Fetching running-config for Device {hn} >>>>".format(hn=hostname))
         config = device.get_config()
         device.close()
         run_conf = config["running"]
         running_config = run_conf.split('\n')
-        
+        return running_config
+
+def get_last_running_config(hostname):
+    if os.path.exists('running-config/'+hostname):
         files = os.listdir('running-config/'+hostname+'/')
         files.sort(reverse=True)
         last_config_file = open('running-config/'+hostname+'/'+files[0], 'r')
         last_running_config = []
         for config in last_config_file:
             last_running_config.append(config.strip('\n'))
-        last_running_config = map(unicode, last_running_config)
-        diff_config_result = set(last_running_config) == set(running_config)
+        last_config_file.close()
+    else:
+        last_running_config = []
+        last_config_file = 'File not found!'
+    return last_running_config,last_config_file.name
+
+def config_diff_result(last_running_config, running_config):
+    return set(last_running_config) == set(running_config)
+
+def write_new_config(hostname, running_config):
+    if not os.path.exists('running-config/'+hostname):
+        print("<<<< Creating Directory for {hn} \n >>>>".format(hn=hostname))
+        os.makedirs('running-config/'+hostname)
+    with open('running-config/'+hostname+'/'+hostname+'-'+time.strftime("%Y-%m-%d--%H-%M")+'.log', 'w') as file:
+        print(">>> Writing to file {file} \n".format(file=file.name))
+        print("="*80,end='\n')
+        for each_line in running_config:
+            file.write(each_line+'\n')
+
+for host in hosts["devices"].keys():
+    hostname = host
+    state = get_host_state(hosts,host)
+    if state == "up":
+        username = get_credentials(hosts,host)[0]
+        password = get_credentials(hosts,host)[1]
+        device = get_napalm_device(hosts,host)
+    elif state == "down":
+        print(">>> Skippping Host {hn} \n".format(hn=hostname))
+        print("="*80)
+        continue
+    else:
+        print(">>> Undefined State for Host {hn} \n".format(hn=hostname))
+        print("="*80)
+        continue
+
+    try:
+        running_config = get_running_config(device)
+        last_running_config = get_last_running_config(hostname)[0]
+        last_config_file = get_last_running_config(hostname)[1]
+        diff_config_result = config_diff_result(last_running_config, running_config)
 
         if diff_config_result == False:
-            if not os.path.exists('running-config/'+hostname):
-                print("<<<< Creating Directory for %s \n >>>>") % hostname
-                os.makedirs('running-config/'+hostname)
-            with open('running-config/'+hostname+'/'+hostname+'-'+time.strftime("%Y-%m-%d--%H-%M")+'.log', 'w') as file:
-                print(">>> Writing to file %s \n") %file.name
-                print("=============================================================================== \n")
-                for each_line in running_config:
-                    file.write(each_line+'\n')
+            write_new_config(hostname, running_config)
         else:
             print(">>> No Changes Since Last Run")
-            print(">>> Last Config File '%s' \n") %files[0]
-            print("=============================================================================== \n")
+            print(">>> Last Config File '{lcf}' \n".format(lcf=last_config_file))
+            print("="*80,end='\n')
 
-    except Exception, e:
-        print("<<<< Unable to Connect to Device %s >>>>") %hostname
-        print("  "+str(type(e)))
-        print("  "+str(e))
-        print("================================================================================ \n")
+    except Exception as e:
+        print("<<<< Unable to Connect to Device {hn} >>>>".format(hn=hostname))
+        print(">>> "+str(type(e)))
+        print(">>> "+str(e))
+        print("="*80,end='\n')
